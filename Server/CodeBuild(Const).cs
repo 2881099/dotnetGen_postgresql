243ea84266142617bbbfe72d8482240379418c5b
2881099 <2881099@qq.com>
2017-06-20 18:05:19 +0800

    1

--- Server/CodeBuild(Const).cs
+++ Server/CodeBuild(Const).cs
@@ -333,7 +333,7 @@ using Npgsql;
 namespace {0}.BLL {{
 
 	public partial class RedisHelper : CSRedis.QuickHelperBase {{
-		public static IConfigurationRoot Configuration {{ get; internal set; }};
+		public static IConfigurationRoot Configuration {{ get; internal set; }}
 		public static void InitializeConfiguration(IConfigurationRoot cfg) {{
 			Configuration = cfg;
 			int port, poolsize, database;
